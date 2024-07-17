6d5e73389d206e3a8093888c33e8460edaa5ddec
Jay Bontawar <125072052+jay26027894@users.noreply.github.com>
2024-07-17 16:59:09 +0530

    website

---
+++ script.js
@@ -0,0 +1,41 @@
+/*<script>
+    // Smooth scrolling function
+    function scrollToSection(sectionId) {
+        document.querySelector(sectionId).scrollIntoView({
+            behavior: 'smooth'
+        });
+    }
+
+    // Event listeners for navigation links
+    document.getElementById('home-link').addEventListener('click', function(event) {
+        event.preventDefault();
+        scrollToSection('#firstsection');
+    });
+
+    document.getElementById('about-link').addEventListener('click', function(event) {
+        event.preventDefault();
+        scrollToSection('#secondsection');
+    });
+
+    document.getElementById('projects-link').addEventListener('click', function(event) {
+        event.preventDefault();
+        scrollToSection('#fourthsection');
+    });
+</script>
+.
+.
+.
+.
+<nav>
+    <div class="left">Portfolio</div>
+    <div class="right">
+        <ul>
+            <li><a id="home-link" href="#firstsection">Home</a></li>
+            <li><a id="about-link" href="#secondsection">About</a></li>
+            <li><a id="projects-link" href="#fourthsection">Projects</a></li>
+        </ul>
+    </div>
+</nav>
+
+*/
+
